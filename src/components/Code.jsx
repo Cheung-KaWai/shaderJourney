90f99da67c9cba12a1a956ead12fea9357a8cba9
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-01-02 13:22:02 +0100

    Add basic code snippet setup

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import recursionData from "../data/recursion.json";
 import { findElementByName } from "../lib/functions";
@@ -7,21 +7,50 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 
 export const Code = () => {
   const { shader } = useShaderStore();
+  const [showCode, setShowCode] = useState(false);
+  const [file, setFile] = useState(0);
 
   const fragment = findElementByName(recursionData, shader.name).fragment ?? null;
   const vertex = findElementByName(recursionData, shader.name).vertex ?? null;
-  console.log(fragment);
+
+  const codes = [fragment, vertex];
+
   return (
-    <CodeContainer>
-      <SyntaxHighlighter language="glsl">{fragment}</SyntaxHighlighter>
-    </CodeContainer>
+    <>
+      <CodeButtons>
+        {showCode && (
+          <>
+            <button onClick={() => setFile(0)}>fragment</button>
+            <button onClick={() => setFile(1)}>vertex</button>
+          </>
+        )}
+        <button onClick={() => setShowCode((prev) => !prev)}>show code</button>
+      </CodeButtons>
+      <CodeContainer $showCode={showCode}>
+        <SyntaxHighlighter language="glsl" useInlineStyles={false} showLineNumbers>
+          {codes[file]}
+        </SyntaxHighlighter>
+      </CodeContainer>
+    </>
   );
 };
 
-const CodeContainer = styled.div`
-  width: 40svw;
-  height: 100svh;
-  background-color: red;
+const CodeButtons = styled.div`
   position: absolute;
   right: 0;
+  z-index: 2;
+  display: flex;
+  gap: 1rem;
+`;
+
+const CodeContainer = styled.div`
+  width: calc((100svw - 20rem) / 2);
+  height: 100svh;
+  position: absolute;
+  right: 0;
+  transform: translateX(${(props) => (props.$showCode ? "0" : "100%")});
+  transition: transform 0.3s ease-in-out;
+  background-color: rgba(255, 255, 255, 0.2);
+  padding: 2.5rem 2rem;
+  backdrop-filter: blur(0.5rem);
 `;
